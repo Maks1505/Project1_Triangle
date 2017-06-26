8432129dd3a1f1da7cf9fbdd025778e083305c5a
Maksim_Bibik <Maksim_Bibik@EPBYMINW0825.minsk.epam.com>
2017-06-26 09:11:17 +0300

    Parametrized method in TestDataValidation class

--- src/test/bibik/project1/validation/TestDataValidation.java
+++ src/test/bibik/project1/validation/TestDataValidation.java
@@ -2,15 +2,38 @@ package test.bibik.project1.validation;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import com.bibik.project1.entity.Point;
+import com.bibik.project1.parser.LineParser;
 import com.bibik.project1.validation.DataValidation;
 
 @RunWith(Parameterized.class)
 public class TestDataValidation {
 
+	private String line;
+	boolean res;
+
+	public TestDataValidation(String line, boolean res) {
+		this.line = line;
+		this.res = res;
+	}
+	
+
+	@Parameters public static List<Object[]> parsedLinesList() {
+		return Arrays.asList(new Object[][] {
+			{"2; 2; 6; 10; 10; 2;", true}, 
+			{"2; 3; 6; x; 7; 2;", true}
+		} 
+		);
+	}
+	
 	@Test
 	public void testThreePointsLieOnStraightLine() {
 		Point pointA = new Point(2, 2);
@@ -21,8 +44,7 @@ public class TestDataValidation {
 
 	@Test
 	public void testSixElementsInRow() {
-		String[] parsedLine = {"2"," 2"," 6"," b"," 10"," 2"};
-		assertTrue(DataValidation.checkSixElementsInRow(parsedLine));
-
+		String[] parsedLine = LineParser.parseLine(this.line);
+		assertTrue(DataValidation.checkSixElementsInRow(parsedLine));  
 	}
 }
