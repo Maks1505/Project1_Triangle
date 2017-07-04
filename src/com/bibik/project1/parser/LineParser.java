07ad26ce3606a7f724aa7ac77c47a3bd00ae38b5
Maksim_Bibik <Maksim_Bibik@EPBYMINW1960.minsk.epam.com>
2017-07-04 13:40:15 +0300

    separator

--- src/com/bibik/project1/parser/LineParser.java
+++ src/com/bibik/project1/parser/LineParser.java
@@ -1,9 +1,10 @@
 package com.bibik.project1.parser;
 
 public class LineParser {
-
+	private static final String SEPARATOR = ";"; 
+	
 	public static /*ArrayList<String>*/ String[] parseLine(String line) {
-		String[] str = line.split(";");
+		String[] str = line.split(SEPARATOR);
 		return str ;
 	}
 }
